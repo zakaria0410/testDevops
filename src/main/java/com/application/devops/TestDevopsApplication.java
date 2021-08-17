ec33956f2ff54bb3606960191997e12cfa100aa1
zakaria0410 <drif.zakaria12@gmail.com>
2021-08-17 10:00:01 +0100

    first install

---
+++ src/main/java/com/application/devops/TestDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.application.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/devops/TestDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
